ec5a22e7c4c8fdc85b8012233ea405c40bff7a25
swiftii <blakereitnauer@gmail.com>
2025-12-19 20:56:31 +0000

    auto-commit for 4dbf9eb3-ce99-42e3-8b84-8fc47666e7d0

--- pro/pro.js
+++ pro/pro.js
@@ -301,7 +301,7 @@
             map = new google.maps.Map(document.getElementById('map'), {
                 center: { lat: 39.8283, lng: -98.5795 },
                 zoom: 4,
-                mapTypeId: 'satellite', // Satellite view by default
+                mapTypeId: 'satellite', // Satellite view by default - ENFORCED
                 disableDefaultUI: true,
                 zoomControl: true,
                 streetViewControl: false,
@@ -312,6 +312,14 @@
                 },
                 tilt: 0
             });
+            
+            // Initialize satellite enforcement with diagnostics
+            if (window.SatelliteUtils) {
+                window.SatelliteUtils.initMapWithSatellite(map, 'Pro');
+            } else {
+                console.log('[Pro] SatelliteUtils not loaded, manually enforcing satellite');
+                map.setMapTypeId('satellite');
+            }
             
             // Initialize Service Area Manager for multi-polygon support
             // Check if ServiceAreaManager is available (loaded from serviceAreaUtils.js)
